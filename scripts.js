c461fcfe3ebfc9212ffbc1cceca191bd310010ac
utahnbron <158127478+utahnbron@users.noreply.github.com>
2024-03-25 07:57:38 -0600

    add function playRound that will play 1 round and alert a win, loss, or tie message

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let playerSelection;
+
 let myArray = [
     "Rock",
     "Paper",
@@ -12,5 +12,26 @@ function getComputerChoice() {
     return computerSelection
 }
 
-console.log(getComputerChoice());
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        alert("You win! Rock beats Scissors");
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        alert("You win! Paper beats Rock");
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        alert("You win! Scissors beats Paper");
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        alert("You lose! Paper beats Rock");
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        alert("You lose! Scissors beats Paper");
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        alert("You lose! Rock beats Scissors");
+    }  else {
+        alert("It's a tie! Play another round");
+    }
+}
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
+console.log(playerSelection);
+console.log(computerSelection);
