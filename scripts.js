2660a06d506cba46d8353ad9104a91d0ce4098e1
utahnbron <158127478+utahnbron@users.noreply.github.com>
2024-03-26 06:21:51 -0600

    Make playerSelection case in-sensitive and create function playRound to play a single round with win, loss, or tie alert message

--- scripts.js
+++ scripts.js
@@ -13,25 +13,27 @@ function getComputerChoice() {
 }
 
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+function playRound(capPlayerSelection, computerSelection) {
+    if (capPlayerSelection === "Rock" && computerSelection === "Scissors") {
         alert("You win! Rock beats Scissors");
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    } else if (capPlayerSelection === "Paper" && computerSelection === "Rock") {
         alert("You win! Paper beats Rock");
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if (capPlayerSelection === "Scissors" && computerSelection === "Paper") {
         alert("You win! Scissors beats Paper");
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    } else if (capPlayerSelection === "Rock" && computerSelection === "Paper") {
         alert("You lose! Paper beats Rock");
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    } else if (capPlayerSelection === "Paper" && computerSelection === "Scissors") {
         alert("You lose! Scissors beats Paper");
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    } else if (capPlayerSelection === "Scissors" && computerSelection === "Rock") {
         alert("You lose! Rock beats Scissors");
     }  else {
         alert("It's a tie! Play another round");
     }
 }
-const playerSelection = "Paper";
+const playerSelection = "PAPER";
+const lowPlayerSelection =  playerSelection.toLowerCase();
+const capPlayerSelection = lowPlayerSelection.charAt(0).toUpperCase() + lowPlayerSelection.slice(1);
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
-console.log(playerSelection);
+console.log(playRound(capPlayerSelection,computerSelection));
+console.log(capPlayerSelection);
 console.log(computerSelection);
