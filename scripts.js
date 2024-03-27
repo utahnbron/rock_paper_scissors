c49f5a3596b13c01c05c0e10f31a95ef3526d9e7
utahnbron <158127478+utahnbron@users.noreply.github.com>
2024-03-27 13:25:27 -0600

    this is the point where i felt like i broke everything, i'm going to go back and review the functions basics lesson and give this another go from the beginning

--- scripts.js
+++ scripts.js
@@ -1,39 +1,61 @@
-
 let myArray = [
     "Rock",
     "Paper",
     "Scissors"
 ];
 
-
-
 function getComputerChoice() {
     let computerSelection = myArray[Math.floor(Math.random()*myArray.length)];
     return computerSelection
 }
-let playerSelection = prompt("Round 1 - Rock, Paper, or Scissors? - 1...2...3 shoot!")
 
 
-function playRound(capPlayerSelection, computerSelection) {
-    if (capPlayerSelection === "Rock" && computerSelection === "Scissors") {
-        alert("You win! Rock beats Scissors");
-    } else if (capPlayerSelection === "Paper" && computerSelection === "Rock") {
-        alert("You win! Paper beats Rock");
-    } else if (capPlayerSelection === "Scissors" && computerSelection === "Paper") {
-        alert("You win! Scissors beats Paper");
-    } else if (capPlayerSelection === "Rock" && computerSelection === "Paper") {
-        alert("You lose! Paper beats Rock");
-    } else if (capPlayerSelection === "Paper" && computerSelection === "Scissors") {
-        alert("You lose! Scissors beats Paper");
-    } else if (capPlayerSelection === "Scissors" && computerSelection === "Rock") {
-        alert("You lose! Rock beats Scissors");
-    }  else {
-        alert("It's a tie! Play another round");
+function playGame(capPlayerSelection, computerSelection) {
+
+ 
+    let playerScore = 0;
+    let computerScore = 0;
+
+    
+    let playerSelection = prompt("Rock, Paper, or Scissors? - 1...2...3 shoot!")
+    const lowPlayerSelection =  playerSelection.toLowerCase();
+    const capPlayerSelection = lowPlayerSelection.charAt(0).toUpperCase() + lowPlayerSelection.slice(1);
+    const computerSelection = getComputerChoice();
+
+    function playRound(capPlayerSelection, computerSelection) {
+        if (capPlayerSelection === "Rock" && computerSelection === "Scissors") {
+            alert("You win! Rock beats Scissors");
+        return playerScore++;
+        } else if (capPlayerSelection === "Paper" && computerSelection === "Rock") {
+            alert("You win! Paper beats Rock");
+            return playerScore++;
+        } else if (capPlayerSelection === "Scissors" && computerSelection === "Paper") {
+            alert("You win! Scissors beats Paper");
+            return playerScore++;
+        } else if (capPlayerSelection === "Rock" && computerSelection === "Paper") {
+            alert("You lose! Paper beats Rock");
+            return computerScore++;
+        } else if (capPlayerSelection === "Paper" && computerSelection === "Scissors") {
+            alert("You lose! Scissors beats Paper");
+            return computerScore++;
+        } else if (capPlayerSelection === "Scissors" && computerSelection === "Rock") {
+            alert("You lose! Rock beats Scissors");
+            return computerScore++;
+        }  else {
+            alert("It's a tie! Play another round");
+        }
+        
     }
 }
-const lowPlayerSelection =  playerSelection.toLowerCase();
-const capPlayerSelection = lowPlayerSelection.charAt(0).toUpperCase() + lowPlayerSelection.slice(1);
-const computerSelection = getComputerChoice();
-console.log(playRound(capPlayerSelection,computerSelection));
-console.log(capPlayerSelection);
+
+console.log(playGame);
+// console.log(capPlayerSelection);
 console.log(computerSelection);
+console.log(playerScore);
+console.log(computerScore);
+
+console.log(playGame);
+// console.log(capPlayerSelection);
+console.log(computerSelection);
+console.log(playerScore);
+console.log(computerScore);
